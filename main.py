3edf3b1bf68c61b99dc579e3f8934c89e84354be
ktfr <112982272+ktfr@users.noreply.github.com>
2024-03-04 23:51:00 -0700

    Created main file

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk() #instantiate an instance of a window
+window.geometry("500x500")
+window.title("Rock, Paper, Scissors")
+
+window.config(background="BLACK")
+
+window.mainloop() 
