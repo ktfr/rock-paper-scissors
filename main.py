bf53b4bb49e5feaa0701972853c966fddb58b8db
ktfr <112982272+ktfr@users.noreply.github.com>
2024-03-12 22:58:32 -0600

    Add methods to determine and show results

--- main.py
+++ main.py
@@ -12,28 +12,42 @@ class Game:
         self.start_button.pack()
 
     def start_game(self):
-        self.start_button.destroy()  # Remove the start button
+        self.start_button.destroy() 
+        self.create_select_label()
         self.create_choice_buttons()
+
+    def create_select_label(self):
+        self.select_label = Label(self.master, text="Please select your weapon:")
+        self.select_label.pack()
     
     def create_choice_buttons(self):
-        self.rock_button = Button(self.master, text="Rock", command=lambda: [self.show_choice("Rock"), self.show_opponent_choice()])
+        self.rock_button = Button(self.master, text="Rock", command=lambda: [self.show_choice("Rock"), 
+                                                                             self.show_opponent_choice(), 
+                                                                             self.calculate_final_result()])
         self.rock_button.pack()
 
-        self.paper_button = Button(self.master, text="Paper", command=lambda: [self.show_choice("Paper"), self.show_opponent_choice()])
+        self.paper_button = Button(self.master, text="Paper", command=lambda: [self.show_choice("Paper"), 
+                                                                               self.show_opponent_choice(),
+                                                                               self.calculate_final_result()])
         self.paper_button.pack()
 
-        self.scissors_button = Button(self.master, text="Scissors", command=lambda: [self.show_choice("Scissors"), self.show_opponent_choice()])
+        self.scissors_button = Button(self.master, text="Scissors", command=lambda: [self.show_choice("Scissors"),
+                                                                                     self.show_opponent_choice(),
+                                                                                     self.calculate_final_result()])
         self.scissors_button.pack()
 
     def show_choice(self, choice):
+        self.user_choice = choice 
         self.remove_choice_buttons()
-        self.choice_label = Label(self.master, text="You chose " + choice)
-        self.choice_label.pack()
+        self.select_label.destroy()
+        self.user_choice_label = Label(self.master, text="You chose " + choice)
+        self.user_choice_label.pack()
     
     def show_opponent_choice(self):
+        global options, random_option
         options = ["Rock", "Paper", "Scissors"]
-        random_option = random.choice(options)
-        self.opponent_label = Label(self.master, text="Your opponent chose " + random_option)
+        self.opponent_choice = random.choice(options)  
+        self.opponent_label = Label(self.master, text="Your opponent chose " + self.opponent_choice)
         self.opponent_label.pack()
 
     def remove_choice_buttons(self):
@@ -41,11 +55,30 @@ class Game:
         self.paper_button.destroy()
         self.scissors_button.destroy()
 
+    def calculate_final_result(self):
+        if self.user_choice == self.opponent_choice:
+            result = "It's a tie!"
+        elif (self.user_choice == "Rock" and self.opponent_choice == "Scissors") or \
+             (self.user_choice == "Paper" and self.opponent_choice == "Rock") or \
+             (self.user_choice == "Scissors" and self.opponent_choice == "Paper"):
+            result = "You win!"
+        else:
+            result = "You lose!"
+        self.show_final_result(result)
 
+    def show_final_result(self, result):
+        self.result_label = Label(self.master, text=result)
+        self.result_label.pack()
+    
+    def restart_game(self):
+        pass
+
+# Create window for Game
 window = Tk() 
-window.geometry("500x500")
+window.geometry("200x300")
 window.title("Rock, Paper, Scissors")  
 
+# Create a new Game instance
 new_game = Game(window)
 
 window.mainloop()
